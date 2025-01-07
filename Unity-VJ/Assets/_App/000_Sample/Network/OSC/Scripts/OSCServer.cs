e5f7db19da308bf78b7d1e81ce4dc769d850b013
Black Pomeranian <ryo.roma.16@gmail.com>
2025-01-07 22:51:41 +0900

    OSCの受信を整理

--- Unity-VJ/Assets/_App/000_Sample/Network/OSC/Scripts/OSCServer.cs
+++ Unity-VJ/Assets/_App/000_Sample/Network/OSC/Scripts/OSCServer.cs
@@ -1,23 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using UniRx;
 using UnityEngine;
 
 public class OSCServer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public IReadOnlyReactiveProperty<float> B1 => _b1;
+    private readonly ReactiveProperty<float> _b1 = new FloatReactiveProperty();
+
+    public IReadOnlyReactiveProperty<float> B2 => _b2;
+    private readonly ReactiveProperty<float> _b2 = new FloatReactiveProperty();
+
+    public IReadOnlyReactiveProperty<float> B3 => _b3;
+    private readonly ReactiveProperty<float> _b3 = new FloatReactiveProperty();
+
+    public IReadOnlyReactiveProperty<float> S1 => _s1;
+    private readonly ReactiveProperty<float> _s1 = new FloatReactiveProperty();
+    public void SetB1(float value)
     {
-        
+        _b1.Value = value;
+        Debug.Log(_b1.Value);
+
+    }
+    public void SetB2(float value)
+    {
+        _b2.Value = value;
+        Debug.Log(_b2.Value);
+
+    }
+    public void SetB3(float value)
+    {
+        _b3.Value = value;
+        Debug.Log(_b3.Value);
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetS1(float value)
     {
-        
+        _s1.Value = value;
+        Debug.Log(_s1.Value);
+
     }
 
-    public void Test(float value)
-    {
-        Debug.Log("Value: " + value);
-    }
 }
