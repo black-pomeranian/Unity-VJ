22b0aaa46116b79d97ecc24619e426a1f01fd5c1
Black Pomeranian <ryo.roma.16@gmail.com>
2025-01-12 18:20:06 +0900

    オブジェクトも緑にできるように変更

--- Unity-VJ/Assets/_App/000_Sample/Core/Manager/Scripts/MaskManager.cs
+++ Unity-VJ/Assets/_App/000_Sample/Core/Manager/Scripts/MaskManager.cs
@@ -9,13 +9,20 @@ public class MaskManager : MonoBehaviour
     [SerializeField] private ParameterManager parameterManager;
     [SerializeField] Material greenSkyBox;
     [SerializeField] Material greenMaterial;
+    [SerializeField] GameObject maskedObject1;
+
     private Material defaultSkyBox;
 
+    private MeshRenderer defaultRenderer1;
+    private Material defaultMaterial1;
 
     void Start()
     {
 
         defaultSkyBox = RenderSettings.skybox;
+
+        defaultRenderer1 = maskedObject1.GetComponent<MeshRenderer>();
+        defaultMaterial1 = defaultRenderer1.material;
 
         // CurrentMaskIndexの値を監視し、変化時にOnIndexChangedを呼び出す
         parameterManager.CurrentMaskIndex
@@ -36,20 +43,37 @@ public class MaskManager : MonoBehaviour
         {
             case 0:
                 RenderSettings.skybox = defaultSkyBox;
-
+                defaultRenderer1.material = defaultMaterial1; 
                 break;
             case 1:
                 RenderSettings.skybox = defaultSkyBox;
+                defaultRenderer1.material = greenMaterial;
 
                 break;
             case 2:
                 RenderSettings.skybox = greenSkyBox;
+                defaultRenderer1.material = defaultMaterial1;
+
 
                 break;
             default:
                 Debug.Log($"Mask Index is {newIndex}: Default Action");
                 // デフォルトの処理
                 break;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        DestroyMaterial(defaultSkyBox);
+        DestroyMaterial(greenMaterial);
+    }
+
+    private void DestroyMaterial(Material material)
+    {
+        if (material != null)
+        {
+            Destroy(material);
         }
     }
 }
