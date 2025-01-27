bcd8fc8c18a4b0e8645ea425d814de13cfef1e45
Black Pomeranian <ryo.roma.16@gmail.com>
2025-01-27 23:37:32 +0900

    オブジェクトにマテリアルの設定

--- Unity-VJ/Assets/_App/000_Basic/Mask/Scripts/MaskController.cs
+++ Unity-VJ/Assets/_App/000_Basic/Mask/Scripts/MaskController.cs
@@ -4,40 +4,68 @@ using UnityEngine;
 
 public class MaskController : MonoBehaviour
 {
-
     [SerializeField] Material greenSkyBox;
-    [SerializeField] Material greenMaterial;
-    [SerializeField] GameObject maskedObject1;
+    [SerializeField] Material renderMaterial;
+    [SerializeField] private List<GameObject> maskedObjects;
 
+    private List<Material> defaultMaterials = new List<Material>();
     private Material defaultSkyBox;
 
-    private MeshRenderer defaultRenderer1;
-    private Material defaultMaterial1;
-
-    // Start is called before the first frame update
     void Start()
     {
+        // デフォルトのスカイボックスを保存
         defaultSkyBox = RenderSettings.skybox;
 
-        defaultRenderer1 = maskedObject1.GetComponent<MeshRenderer>();
-        defaultMaterial1 = defaultRenderer1.material;
+        // 各オブジェクトのデフォルトマテリアルをリストに格納
+        foreach (GameObject obj in maskedObjects)
+        {
+            MeshRenderer renderer = obj.GetComponent<MeshRenderer>();
+            if (renderer != null)
+            {
+                defaultMaterials.Add(renderer.material);
+            }
+        }
     }
 
     public void SetMask1()
     {
         RenderSettings.skybox = defaultSkyBox;
-        defaultRenderer1.material = defaultMaterial1;
+        ApplyDefaultMaterials();
     }
 
     public void SetMask2()
     {
         RenderSettings.skybox = defaultSkyBox;
-        defaultRenderer1.material = greenMaterial;
+        ApplyMaterialToAll(renderMaterial);
     }
 
     public void SetMask3()
     {
         RenderSettings.skybox = greenSkyBox;
-        defaultRenderer1.material = defaultMaterial1;
+        ApplyDefaultMaterials();
+    }
+
+    private void ApplyDefaultMaterials()
+    {
+        for (int i = 0; i < maskedObjects.Count; i++)
+        {
+            MeshRenderer renderer = maskedObjects[i].GetComponent<MeshRenderer>();
+            if (renderer != null && i < defaultMaterials.Count)
+            {
+                renderer.material = defaultMaterials[i];
+            }
+        }
+    }
+
+    private void ApplyMaterialToAll(Material material)
+    {
+        foreach (GameObject obj in maskedObjects)
+        {
+            MeshRenderer renderer = obj.GetComponent<MeshRenderer>();
+            if (renderer != null)
+            {
+                renderer.material = material;
+            }
+        }
     }
 }
